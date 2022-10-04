274db8f042955d205d904f75e3ddbd09fbea9840
Melih Taşçı <melihhtasci@gmail.com>
2022-10-04 15:58:25 +0300

    added jwt for auth

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,11 +1,22 @@
+
+using Application.Common.Interfaces.Authentication;
+using Application.Common.Interfaces.Services;
+using Infrastructure.Authentication;
+using Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services, 
+        ConfigurationManager configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         return services;
     }
 }
